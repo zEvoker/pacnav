023b822e9481811f89203b4646a52503a9d550ab
evoker <jbsimon03@gmail.com>
2024-05-15 22:56:52 +0530

    updates

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,20 +9,56 @@ import mspacman from '../../assets/images/mspacman.png'
 import pacjr from '../../assets/images/pacjr.png'
 import babypac from '../../assets/images/babypac.png'
 import pacmancoin from '../../assets/images/pacmancoin.png'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Footer = () => {
     const descs=["begins each level moving at the same speed as all of the other ghosts, but after you've eaten a certain number of dots, he begins to speed up.","seems to have a tendency to go around blocks in an anticlockwise direction unlike Blinky and Clyde who seem to prefer going clockwise.","is dangerous because he's unpredictable. Given the same choices, he will often take different turns at different times.","is either short-sighted or stupid. He will often turn off rather than approach you. His heart doesn't seem to be in it at all.","is the titular protagonist of the Pac-Man series. Residing in Pac-Land, he regularly favors eating various types of Pac-Dots.",", also known as Pepper, is Pac-Man's wife and sidekick and is the mother of Jr. Pac-Man and Baby Pac-Man.","is the first-born son of Pac-Man and Ms. Pac-Man. During his adventures, he fell in love with Yum-Yum, Blinky's daughter.","is the second-born, infant child of Pac-Man and Ms. Pac-Man, and the younger sibling to Jr. Pac-Man."];
     const names=["Blinky","Pinky","Inky","Clyde"];
+
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState([]);
+    const [delta, setDelta] = useState(50);
+    const [idx, setIdx] = useState(0);
+    const descStr = "As Pac-Nav captures the essence of the elements from the 80's arcade game, here are some interesting trivia for its super fans "
+    const txt = [[...descStr]];
+    const titleStr = "PAC-MAN";
+    const titleArr= [[...titleStr],['T','R','I','V','I','A']]
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let curText = isDeleting ? txt[idx].slice(0, text.length - 1) : txt[idx].slice(0, text.length + 1);
+        setText(curText);
+        if (!isDeleting && curText.length === txt[idx].length) {
+            setIsDeleting(true);
+            setDelta(20);
+        } else if (isDeleting && curText.length === 0) {
+            setIsDeleting(false);
+            setIdx((1 + idx) % 1);
+            setDelta(100);
+        }
+    }
 
     return(
         <div className="footer">
             <div className="scrollpac"></div>
             <div className="meet">
                 <div className="meetfam">
-                    <span>PAC-MAN</span><span>TRIVIA</span>
+                    <div className='meetfirst'>
+                    {titleArr[0].map((letter, index) => (<span key={index}>{letter}</span>))}
+                    </div>
+                    <div className='meetsec'>
+                    {titleArr[1].map((letter, index) => (<span key={index}>{letter}</span>))}
+                    </div>
                 </div>
                 <div className="meetdesc">
-                    <span>As Pac-Nav captures the essence of the elements from the 80’s arcade game, here are some interesting trivia for its super fans.</span>
+                    <span>{text}</span><span className='blinker'>&#95;</span>
                 </div>
                 <div className="row">
                 <Coin backimg={pinky} name={names[1]} desc={descs[1]} clr="#FFABE7"/>
